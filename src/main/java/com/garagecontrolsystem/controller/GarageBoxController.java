c7da0acc82fb50a8ed00c8f9fb69c3cc00290134
Lúcio Hermano Batista <prluciohermano@gmail.com>
2022-09-09 10:59:10 -0300

    Correção do var final

--- src/main/java/com/garagecontrolsystem/controller/GarageBoxController.java
+++ src/main/java/com/garagecontrolsystem/controller/GarageBoxController.java
@@ -10,10 +10,6 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,12 +22,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.garagecontrolsystem.dto.CategoriaDTO;
 import com.garagecontrolsystem.dto.GarageBoxDTO;
 import com.garagecontrolsystem.entity.GarageBoxModel;
 import com.garagecontrolsystem.service.GarageBoxService;
 
-import lombok.var;
 
 @CrossOrigin("*")
 @RestController
